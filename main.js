b7d49e1f83b6cf18a1f0c7f59cd679b55061a0d3
Jusa Eronen <juza.eronen@gmail.com>
2022-01-13 18:30:54 +0200

    kesken

--- main.js
+++ main.js
@@ -5,19 +5,20 @@ import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/js
 const scene = new THREE.Scene();
 scene.background = new THREE.TextureLoader().load("./images/space.jpg");
 const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+camera.position.set(15, 5, 0);
+camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGLRenderer({canvas: document.querySelector("#bg"), antialias: true});
 renderer.setPixelRatio(window.devicePixelRatio);
-
 
 
 //Lights
 const ambientLight = new THREE.AmbientLight(0x888888);
 const directionalLight = new THREE.DirectionalLight(0xfdfcf0, 1);
-directionalLight.position.set(30, 0, 0);
+directionalLight.position.set(30, -20, -45);
 scene.add(ambientLight, directionalLight);
 
 //Earth
-const earthGeometry = new THREE.SphereGeometry(5, 50, 50);
+const earthGeometry = new THREE.SphereGeometry(10, 50, 50);
 const earthMaterial = new THREE.MeshPhongMaterial({
     map: new THREE.TextureLoader().load("./images/earth.jpg"),
     normalMap: new THREE.TextureLoader().load("./images/earthNormalMap.jpg"),
@@ -26,9 +27,10 @@ const earthMaterial = new THREE.MeshPhongMaterial({
     shininess: 15
 });
 const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+earth.position.set(0,0,0);
 
 //Clouds
-const cloudGeometry = new THREE.SphereGeometry(5.1, 50, 50);
+const cloudGeometry = new THREE.SphereGeometry(10.1, 50, 50);
 const cloudMaterial = new THREE.MeshPhongMaterial({
     map: new THREE.TextureLoader().load("./images/clouds.jpg"),
     transparent: true,
@@ -53,11 +55,11 @@ scene.add(moon)
 
 
 //Controls
-const controls = new OrbitControls(camera, renderer.domElement);
+//const controls = new OrbitControls(camera, renderer.domElement);
 
 //2D Grid
-//const gridHelper = new THREE.GridHelper(200, 50);
-//scene.add(gridHelper);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
 
 //Resize canvas on display resize
 function canvasResizeToDisplay(force) {
@@ -69,24 +71,6 @@ function canvasResizeToDisplay(force) {
         renderer.setSize(width, height, false);
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
-
-        if (width < 1100 || height > width) {
-
-            camera.position.set(10, 0, 0);
-            controls .target.set(0,5,0);
-            controls.update();
-            controls.enabled = false;
-
-        } else if (width > 1100) {
-
-            camera.position.set(30, 0, 0);
-            camera.up = new THREE.Vector3(0, 3, 1);
-            camera.lookAt(new THREE.Vector3(0, 0, 0));
-            controls.target.set(9, 5, -20);
-            controls.update();
-            controls.enabled = false;
-
-        }
     }
 }
 
